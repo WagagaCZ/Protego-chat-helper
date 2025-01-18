1763df56ef2813e4cac47ad6d0f4c9e6602c7247
Matěj Chalupa <125280779+WagagaCZ@users.noreply.github.com>
2025-01-18 14:45:12 +0100

    Wagaga cz patch 1 (#1)
    * Fix mid-word catches
    
    * Update Protego-Highlight.user.js
    
    Fix performance; Added submitable pop-up for user input
    
    * Update README.md
    
    Update ReadMe

--- Protego-Highlight.user.js
+++ Protego-Highlight.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Protego - Highlight
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Zvýrazní jméno na Protegu!
 // @author       Wagaga
 // @match        https://www.protego.cz/system/chat/index.php*
@@ -11,11 +11,14 @@
 
 (async function() {
     'use strict';
-    const regex = /((Svetl|Ston|Světl)\w*)/gi;
-    let elements = document.getElementsByClassName("m-co");
+    let input = window.prompt("Zadej jména na zvýraznění oddělená mezerou")
+    const betterInput = input.replace(/(\s+)/gi, '|')
+    const regex = new RegExp("([^>] )((" + betterInput + ")\\w*)", "gi");
+    let elements;
+    console.error(regex);
     let msgBoard = document.getElementById("messBg");
     let hit;
-while(1){
+while(1){ 
     console.log("While loop.");
     msgBoard = document.getElementById("messBg");
     elements = msgBoard.getElementsByTagName("p");
@@ -23,8 +26,10 @@ while(1){
     console.log(elements);
     for(let i = 0; i < elements.length; i++){
         console.log(elements.item(i).innerHTML.match(regex));
-        hit = elements.item(i).innerHTML.replace(regex, '<b style="background-color:#fe8a18; color:Black;">$1</b>');
-        elements.item(i).innerHTML = hit;
+        if(elements.item(i).innerHTML.match(regex)) {
+            hit = elements.item(i).innerHTML.replace(regex, '$1<b style="background-color:#fe8a18; color:Black;">$2</b>');
+            elements.item(i).innerHTML = hit;
+        }
     }
 }
 })();
